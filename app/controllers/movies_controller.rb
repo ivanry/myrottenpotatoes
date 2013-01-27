a0d2e1c043f46a617ab69d7f986252e37598c5e6
ivan.rybnikov <ivan.rybnikov@gmail.com>
2013-01-27 23:44:28 +0300

    "show" action added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,11 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
 
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+                            # will render app/views/movies/show.html.haml by default
+  end
 
 
 end

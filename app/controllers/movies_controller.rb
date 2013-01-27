e1fb77d5747df7b8184f67a09e7cc6c1f121ed03
ivan.rybnikov <ivan.rybnikov@gmail.com>
2013-01-28 00:27:26 +0300

    debug examples added: 1. rising exceptions 2. logger 3. printf style

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,15 @@
 class MoviesController < ApplicationController
 
   def index
+    logger.debug "Test log message"
     @movies = Movie.all
   end
 
   def show
+
+    #debug example
+    #raise params.inspect
+
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
                             # will render app/views/movies/show.html.haml by default
